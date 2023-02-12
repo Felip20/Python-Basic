adf1cfe3dcf181f52c20b5bebc55b0821a195836
wathone <mg.wathonekhant@gmail.com>
2023-02-12 22:00:40 +0630

    format string

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # name = input('Name :');
 # print(name);
 
-PI = 3.14;
-r= int(input('radius:'));
+# PI = 3.14;
+# r= int(input('radius:'));
 
-area = PI*r**2;
+# area = PI*r**2;
 
-print(area);2
+# print(area);2
+
+age = 20;
+
+print(f'my age is {age}');
+
