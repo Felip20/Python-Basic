49cb16f8bdcb9c147341cfee20d3f9b9c0caa717
wathone <mg.wathonekhant@gmail.com>
2023-02-12 22:22:26 +0630

    if condition

--- main.py
+++ main.py
@@ -15,3 +15,20 @@
 # print(5>1 and 5>2);
 
 # print(5>1 or 5>10);
+
+# age = int(input('age:'));
+
+# if age < 18:
+#     print('young');
+# elif age > 18 and age < 30:
+#     print('normal');
+# else :
+#     print('old');
+
+tired = input('are you ok? "y/n"');
+if tired == 'n':
+    print('rest well');
+elif tired == 'y':
+    print('go to work');
+else :
+    print('please enter y/n');
