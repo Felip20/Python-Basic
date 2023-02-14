8b28d077bb7a4f0daef253ae72684a6d709f80c4
wathone <mg.wathonekhant@gmail.com>
2023-02-15 00:34:44 +0630

    dictionary usage

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+person = {
+    'name': 'wathone',
+    'age': 21
+}
+person['goat']='messi'
+# print(person);
+
+# if 'goat' in person:
+#     print('its work');
+# else:
+#     print('idk');
+
+pskey=list(person.keys())
+
+print(pskey);
+
+psvalue=list(person.values())
+
+print(psvalue);
