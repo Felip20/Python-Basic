82504c459fbde731f38773c41a0ae543ef5ac1ac
wathone <mg.wathonekhant@gmail.com>
2023-02-17 04:11:39 +0630

    lambda

---
+++ lambda.py
@@ -0,0 +1,8 @@
+# def num(n1,n2):
+#     return n1+n2;
+
+# print(num(4,5));
+
+num=lambda n1,n2:n1+n2
+
+print(num(4,5));
--- map.py
+++ map.py
@@ -1,8 +1,8 @@
 nums = [2,3,6,5,8,4,9,10]
 
-def function(num):
-    return num*2;
+# def function(num):
+#     return num*2;
 
-num = list(map(function,nums));
+num = list(map(lambda num:num*2,nums));
 
 print(num);
